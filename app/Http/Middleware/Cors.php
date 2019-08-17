435a58166202c3cadc8dbf211ab8e0ffa515b50e
Manuel Barrios <mbarrios@Manuels-MBP.utopia.net>
2019-08-17 12:02:02 -0300

    api

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,71 @@
+<?php namespace App\Http\Middleware;
+
+use App\Entities\User;
+use Closure;
+
+class Cors
+{
+
+    public function handle($request, Closure $next)
+    {
+
+        // ALLOW OPTIONS METHOD
+
+//        $headers = [
+//            'Access-Control-Allow-Origin' => '*',
+//            'Access-Control-Allow-Headers' => 'Origin, Content-Type',
+//            'Access-Control-Allow-Credentials: true',
+//
+//        ];
+
+
+        //$token = $request->headers->all();
+        //$token = '$2y$10$WqqbMZ/W4njUOe3DQDTBy.OHXdzY/QdXbxIT1Pc.jGSa3g.b2qC7G';
+
+//    if(isset($token['x-csrf-token']))
+//    {
+//        $user = User::where('token', $token['x-csrf-token'])->get();
+//
+//        if ($user->count() != 0) {
+//            Auth::loginUsingId($user->first()->id);
+//
+//            header('Access-Control-Allow-Origin:  *');
+//            header('Access-Control-Allow-Credentials: true');
+//            header('Acces-Control-Allow-Headers: Origin, Content-Type ');
+//
+//            return $next($request);
+//        }
+//        return response()->json('invalid Token');
+//    }
+//    else
+//    {
+//        return response()->json('Token not found');
+//    }
+
+
+        // if($request->server('HTTP_HOST') == 'localhost')
+        return $next($request)
+            // ->header("Content-Type: application/json; charset=UTF-8");
+            // ->header("Access-Control-Allow-Headers: Accept-Encoding, X-Requested-With, Content-Type, Origin, Accept, Authenticationtoken");
+            ->header('Access-Control-Allow-Origin', '*')
+            //->header('Access-Control-Allow-Credentials', 'true')
+            // ->header('Acces-Control-Allow-Headers: Origin, Content-Type ')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
+
+
+        /*
+
+        if ($request->getMethod() != "OPTIONS") {
+            return $next($request);
+        }
+
+        $response = $next($request);
+
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+*/
+
+    }
+}
